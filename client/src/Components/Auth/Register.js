f3f086ff1d25b157a44143adaea811cc10bcd7ec
test <eb2ray@gmail.com>
2021-01-08 00:00:52 +0200

    Added a regsitration form

--- client/src/Components/Auth/Register.js
+++ client/src/Components/Auth/Register.js
@@ -1,11 +1,203 @@
-import React from 'react'
+import React from 'react';
+import { Form, Input, Tooltip, Row, Col, Button, Typography } from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import styles from './Register.module.css';
+
+const { Title } = Typography;
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 
 const Register = () => {
-    return (
-        <div>
-            <h2>Register Page</h2>
-        </div>
-    )
-}
+  const [form] = Form.useForm();
 
-export default Register
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <>
+      <Form
+        className={styles.Register}
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        scrollToFirstError
+      >
+        <Form.Item>
+          <Title className="title" level={3}>
+            Register an account
+          </Title>
+        </Form.Item>
+        <Form.Item
+          name="name"
+          label={
+            <span>
+              Name&nbsp;
+              <Tooltip title="Enter your first name first then your last name?">
+                <QuestionCircleOutlined />
+              </Tooltip>
+            </span>
+          }
+          rules={[
+            {
+              required: true,
+              message: 'Please input your name!',
+              whitespace: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="username"
+          label={
+            <span>
+              Username&nbsp;
+              <Tooltip title="Username must be 6 characters long and must have an uppercase letter, number and symbols">
+                <QuestionCircleOutlined />
+              </Tooltip>
+            </span>
+          }
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username name!',
+              whitespace: false,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  'The two passwords that you entered do not match!'
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="Captcha"
+          extra="We must make sure that your are a human."
+        >
+          <Row gutter={8}>
+            <Col span={12}>
+              <Form.Item
+                name="captcha"
+                noStyle
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input the captcha you got!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Button>Get captcha</Button>
+            </Col>
+          </Row>
+        </Form.Item>
+
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject('Should accept agreement'),
+            },
+          ]}
+          {...tailFormItemLayout}
+        >
+          {/* <Checkbox>
+            I have read the <a href="">agreement</a>
+          </Checkbox> */}
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+export default Register;
--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { Button, Space, Typography } from 'antd';
+const { Title } = Typography;
 
 const Home = () => {
     return (
-        <div>
-            <h2>Home Page</h2>
+      <section style={{ textAlign: 'center' }}>
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <Title>Inventory Management App</Title>
+            <p className="lead">
+              Use this app to add, edit, delete and search items in the
+              inventory
+            </p>
+            <Space>
+              <Button type="primary">
+                <Link to="/register">Register</Link>
+              </Button>
+              <Button type="primary">
+                <Link to="/login">Login</Link>
+              </Button>
+            </Space>
+          </div>
         </div>
-    )
+      </section>
+    );
 }
 
 export default Home
