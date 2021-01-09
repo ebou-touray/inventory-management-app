436904e362d6efbf4248d245eb45ad1f6c2671dc
test <eb2ray@gmail.com>
2021-01-09 23:39:29 +0200

    Added a login form

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined, LoginOutlined } from '@ant-design/icons';
+import styles from './Login.module.css';
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login Page</h2>
-        </div>
-    )
-}
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
 
-export default Login
+  return (
+    <Form
+      name="normal_login"
+      className={styles.Login}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        label="Username"
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        Or <a href="/register">register now!</a>
+      </Form.Item>
+    </Form>
+  );
+};
+export default Login;
